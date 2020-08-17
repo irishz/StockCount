ae13e1a0fb73f7b561bcf6ba37e84303e261a78f
Jetsada Prasitsuwan <ice-jetsada@hotmail.com>
2020-08-17 12:26:19 +0700

    update 17/08/2020 12:25 PM
    released

--- App.js
+++ App.js
@@ -7,13 +7,11 @@ import {
   Text,
   ActivityIndicator,
   TextInput,
+  Button,
+  RefreshControl,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
@@ -24,6 +22,8 @@ export default class App extends Component {
     this.state = {
       isloading: true,
       list: [],
+      sum: 0,
+      refreshing: false,
     };
     this.onSuccess = this.onSuccess.bind(this);
   }
@@ -32,56 +32,104 @@ export default class App extends Component {
     var tempList = e.data.split('+');
     this.setState({
       isloading: false,
-      list: [...this.state.list, {item: tempList[1], qty: tempList[2]}],
+      list: [...this.state.list, {item: tempList[1], qty: tempList[2] * 1}],
     });
   };
 
-  // update = (item, index) => {
-  //   const newArray = [...this.state.list];
-  //   newArray[index].value = item;
-  //   this.setState({list: newArray});
-  // };
+  updateItem(item, i, e) {
+    console.log(e);
+    var array = [...this.state.list];
+    array[i] = {item: item, qty: e * 1};
+    this.setState({
+      list: array,
+    });
+  }
+
+  removeItem(i) {
+    var array = [...this.state.list];
+    array.splice(i, 1);
+    this.setState({
+      list: array,
+    });
+  }
+
+  refresh() {
+    this.setState({
+      isloading: true,
+      list: [],
+      sum: 0,
+      refreshing: true,
+    });
+  }
 
   render() {
-    const total = this.state.list;
-    console.log(total.qty);
-    // console.log('list:' + this.state.list);
+    //sum qty in array
+    const total = this.state.list.reduce((prev, next) => prev + next.qty, 0);
+
     return (
-      <View>
-        <SafeAreaView>
+      <View style={styles.container}>
+        {/* QR Scanner & content*/}
+        <View style={styles.qrscan}>
+          <QRCodeScanner
+            onRead={this.onSuccess}
+            reactivate={true}
+            reactivateTimeout={1000}
+            flashMode={RNCamera.Constants.FlashMode.off}
+          />
+        </View>
+
+        {/* Content */}
+        <SafeAreaView style={styles.safeAreaView}>
           <ScrollView
+            ref={(view) => {
+              this.scrollView = view;
+            }}
+            onContentSizeChange={() =>
+              this.scrollView.scrollToEnd({animated: true})
+            }
             contentInsetAdjustmentBehavior="automatic"
+            refreshControl={
+              <RefreshControl
+                tintColor={'#ff0000'}
+                refreshing={false}
+                onRefresh={this.refresh.bind(this)}
+              />
+            }
             style={styles.scrollView}>
-            <QRCodeScanner
-              onRead={this.onSuccess}
-              reactivate={true}
-              reactivateTimeout={1000}
-              flashMode={RNCamera.Constants.FlashMode.off}
-            />
-            {global.HermesInternal == null ? null : (
-              <View style={styles.engine}>
-                <Text style={styles.footer}>Engine: Hermes</Text>
-              </View>
-            )}
             <View style={styles.body}>
               <View style={styles.sectionContainer}>
                 {this.state.isloading ? (
-                  <View style={styles.listView}>
-                    <Text>กำลังรอการสแกน </Text>
+                  <View style={styles.loading}>
+                    <Text style={styles.sectionDescription}>
+                      กำลังรอการสแกน
+                    </Text>
                     <ActivityIndicator
                       animating={true}
                       size="large"
-                      color="#0000FF"
+                      color="#0066CC"
                     />
                   </View>
                 ) : (
                   this.state.list.map((item, index) => {
                     return (
                       <View style={styles.listView} key={index}>
-                        <Text style={styles.list}>ลำดับที่:{index + 1}</Text>
+                        <Text style={styles.list}>{index + 1}</Text>
                         <Text style={styles.list}>{item.item}</Text>
-                        <Text style={styles.list}>{item.qty}</Text>
-                        {/* <TextInput onChangeText={this.update(item, index)} /> */}
+                        <TextInput
+                          keyboardType="number-pad"
+                          style={styles.input}
+                          onChangeText={(e) => {
+                            this.updateItem(item.item, index, e);
+                          }}>
+                          {item.qty}
+                        </TextInput>
+                        <Button
+                          title="ลบ"
+                          color="#FF4343"
+                          onPress={() => {
+                            this.removeItem(index);
+                          }}
+                        />
                       </View>
                     );
                   })
@@ -90,19 +138,30 @@ export default class App extends Component {
             </View>
           </ScrollView>
         </SafeAreaView>
-        <View style={styles.footer} />
+
+        {/* Footer */}
+        <View style={styles.footer}>
+          <Text style={{color: '#FFF', fontSize: 18}}>รวม: {total}</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  qrscan: {
+    flex: 0.5,
+  },
+  safeAreaView: {
+    flex: 0.45,
   },
   body: {
     backgroundColor: Colors.white,
@@ -111,7 +170,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginTop: 32,
-    paddingHorizontal: 24,
   },
   sectionTitle: {
     fontSize: 24,
@@ -128,22 +186,34 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   footer: {
-    color: Colors.dark,
+    flex: 0.05,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#006AE2',
     fontSize: 12,
     fontWeight: '600',
     padding: 4,
     paddingRight: 12,
-    textAlign: 'right',
   },
   listView: {
     flex: 1,
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: 40,
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 24,
   },
   list: {
     fontSize: 14,
   },
-  input: {
-    borderStyle: 'solid',
-    borderBottomWidth: 1,
+  button: {
+    color: '#ff0000',
+  },
+  loading: {
+    alignItems: 'center',
   },
 });
